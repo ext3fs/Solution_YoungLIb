6c6a5477f04d732cf3d0fb8359b2b363dcd1ec50
ext3fs <ext7fs@gmail.com>
2025-09-11 22:46:14 +0900

    초기 커밋.

---
+++ YoungLIbTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoungLIbTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(YoungLib.Tools.Application.Root);
+        }
+    }
+}
